4b274752c602087ee35ff6641c69c6a45e8096a2
Terry-JohnsonFS <97139342+Terry-JohnsonFS@users.noreply.github.com>
2023-06-04 14:10:37 -0700

    Price checking page created and buttons added working on functionality for buttons on page

--- app/src/main/java/com/example/gamesale/MainActivity.kt
+++ app/src/main/java/com/example/gamesale/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.gamesale
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mainButton = findViewById<Button>(R.id.button)
+        mainButton.setOnClickListener {
+            val intent = Intent(this, SteamLookUp::class.java)
+            startActivity(intent)
+        }
     }
 }

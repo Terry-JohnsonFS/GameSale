448748f213dc94d1f1f86cdd73c71d7926284748
Terry-JohnsonFS <97139342+Terry-JohnsonFS@users.noreply.github.com>
2023-06-04 14:13:51 -0700

    sales times page created and linked with button on the Steam lookup activity

--- app/src/main/java/com/example/gamesale/SteamLookUp.kt
+++ app/src/main/java/com/example/gamesale/SteamLookUp.kt
@@ -1,13 +1,19 @@
 package com.example.gamesale
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 
 class SteamLookUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_steam_look_up)
+
+        val SalesButton = findViewById<Button>(R.id.button2)
+        val intent = Intent(this, SteamSalesTimes::class.java)
+        startActivity(intent)
     }
 
     private fun userinput(): String {
